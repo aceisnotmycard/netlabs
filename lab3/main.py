852847fc5d77528c49706e37463541dd0bb2b2f5
Sergey Bogolepov <sergey@Sergeys-MacBook-Air.local>
2015-10-05 13:34:23 +0600

    sets ftw

--- lab3/main.py
+++ lab3/main.py
@@ -1,4 +1,54 @@
 import socket
+import sys
+import atexit
+
+SOCK = None
+PORT = 0
+BROADCAST_ADDR = '255.255.255.255'
+BORN_MSG = "IBORN"
+LIVE_MSG = "ILIVE"
+EXIT_MSG = "IEXIT"
+
+hosts = set()
+
+
+def send_msg(message: str, address: str, port: int):
+    SOCK.sendto(message.encode("utf8"), (address, port))
+
+
+def list_hosts(hosts: set):
+    print("List of alive hosts")
+    for host in hosts:
+        print(host)
+
+
+@atexit.register
+def say_goodbye():
+    send_msg(EXIT_MSG, BROADCAST_ADDR, PORT)
+
 
 if __name__ == "__main__":
-    sc = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    PORT = sys.argv[-1]
+    SOCK = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    SOCK.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    SOCK.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    SOCK.sendto(EXIT_MSG.encode("utf8"), (BROADCAST_ADDR, PORT))
+    while True:
+        data, addr = SOCK.recvfrom(1024)
+        if data.decode("utf8") == BORN_MSG:
+            # add host to list, display it and say hello
+            hosts.add(addr)
+            list_hosts(hosts)
+            SOCK.sendto(LIVE_MSG.encode("utf8"), addr)
+        elif data.decode("utf8") == LIVE_MSG:
+            # add host to list, display it
+            hosts.add(addr)
+            list_hosts(hosts)
+        elif data.decode("utf8") == EXIT_MSG:
+            # remove host from list and display list again
+            if addr in hosts:
+                hosts.remove(addr)
+            list_hosts(hosts)
+        else:
+            # junk processing
+            pass
