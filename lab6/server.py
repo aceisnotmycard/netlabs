cc03e65aec13c399807dd37c4d48fff300574f02
Sergey Bogolepov <ace.is.not.my.card@gmail.com>
2015-10-24 23:30:37 +0600

    Done with sequence generator

--- lab6/server.py
+++ lab6/server.py
@@ -1,18 +1,39 @@
 import socket
 import argparse
 from hashlib import md5
+import math
 
-
-sequences = []
-iter = 0
+prev_seq = ""
 
 
 def create_ranges(genome_len: int, num_ranges: int):
-    if num_ranges == 1:
-        sequences.append("a" * genome_len)
-    else:
-        trigger = 4**genome_len // (num_ranges-1)
-        generator(length=genome_len, trigger=trigger)
+    sequences = {}
+    offsets = [int]
+    counter = [0]
+    prev_counter = [0]
+
+    # generating starting sequences
+    def seq_gen(length=4, seq="", trigger: int = 1):
+        acgt = ["a", "c", "g", "t"]
+        global prev_seq
+        if length == 0:
+            if counter[0] == 0:
+                prev_seq = seq
+            elif seq == "t" * genome_len:
+                sequences[(prev_seq, counter[0] - prev_counter[0] + 1)] = False
+            elif counter[0] % trigger == 0:
+                sequences[(prev_seq, counter[0] - prev_counter[0])] = False
+                prev_seq = seq
+                prev_counter[0] = counter[0]
+            counter[0] += 1
+        else:
+            for c in acgt:
+                seq_gen(length - 1, seq + c, trigger=trigger)
+
+    trigger = math.ceil(4 ** genome_len / num_ranges)
+    seq_gen(length=genome_len, trigger=trigger)
+    return sequences
+
 
 def main(genome: str, port: int):
     clients_list = []
@@ -30,7 +51,6 @@ def main(genome: str, port: int):
 
 def printer(l, start=""):
     alph = ["a", "c", "g", "t"]
-
     if l == 0:
         # do something here
         pass
@@ -39,25 +59,12 @@ def printer(l, start=""):
             printer(l - 1, start + a)
 
 
-def generator(length=4, seq="", trigger: int = 1):
-    global sequences
-    global iter
-    alph = ["a", "c", "g", "t"]
-    if length == 0:
-        if iter % trigger == 0:
-            sequences.append(seq)
-    else:
-        for a in alph:
-            generator(length - 1, seq + a, trigger=trigger)
-            iter += 1
-
-
 if __name__ == "__main__":
     # parser = argparse.ArgumentParser(description="Cracks given genome")
     # parser.add_argument('genome', type=str, help='Genome to crack', metavar='Genome')
     # parser.add_argument('port', type=int, help='Port to listen to', metavar='Port')
     # args = parser.parse_args()
     # main(args.genome, args.port)
-    create_ranges(genome_len=2, num_ranges=8)
-    for s in sequences:
-        print(s)
+    sequences = create_ranges(genome_len=3, num_ranges=4)
+    for k in sequences:
+        print(k, sequences[k])
